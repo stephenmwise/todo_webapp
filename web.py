88b0a1357c851ac36b0c0a29eb2536a1a3663486
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-08-07 21:10:04 -0400

    Initial Commit

---
+++ web.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+st.title("To-do App")
+st.subheader("General tasks")
+st.write("Create and complete to-do items below.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="",
+              label_visibility="collapsed",
+              placeholder="Add new todo item...",
+              on_change=add_todo,
+              key="new_todo")
